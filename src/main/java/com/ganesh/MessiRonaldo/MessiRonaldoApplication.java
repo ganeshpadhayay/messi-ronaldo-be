b1054dcce5a49c540931c0adc58b3af3778b7e7c
Ganesh Padhayay <ganeshraj020794@gmail.com>
2023-02-17 23:32:01 +0530

    initial commit, BE project setup done with health check api

---
+++ src/main/java/com/ganesh/MessiRonaldo/MessiRonaldoApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.MessiRonaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessiRonaldoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessiRonaldoApplication.class, args);
+    }
+
+}
